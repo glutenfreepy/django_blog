93760df4c2d94304a276f0658071b02e0871ffa2
edg <glutenfreepy@gmail.com>
2024-08-05 11:19:20 -0700

    add view for the form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -47,3 +48,30 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+        else:
+            form = EmailPostForm()
+        return render(
+            request,
+            'blog/post/share.html',
+            {
+                'post': post,
+                'form': form
+            }
+        )
