0eab738bef2cdd5758aad4ed78776106308fb018
edg <glutenfreepy@gmail.com>
2024-08-06 10:17:51 -0700

    add view for sending emails

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
@@ -57,6 +58,7 @@ def post_share(request, post_id):
         id=post_id,
         status=Post.Status.PUBLISHED
     )
+    sent = False
 
     if request.method == 'POST':
         # Form was submitted
@@ -64,7 +66,24 @@ def post_share(request, post_id):
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_ablsolute_url(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
         else:
             form = EmailPostForm()
         return render(
@@ -72,6 +91,7 @@ def post_share(request, post_id):
             'blog/post/share.html',
             {
                 'post': post,
-                'form': form
+                'form': form,
+                'sent': sent
             }
         )
